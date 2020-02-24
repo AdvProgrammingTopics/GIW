49746698cd6200ca09eead387bd653487f1a274d
iveyjustinlee <iveyjustinlee@gmail.com>
2020-02-23 19:03:08 -0500

    Add files via upload

---
+++ LeadEntry.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GIW
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LeadEntry : ContentPage
+	{
+		public LeadEntry ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GIW
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Label label = new Label
+            {
+                Text = "https://products.ksb.com/global/"
+            };
+            
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) =>
+            {
+                Device.OpenUri(new Uri(((Label)s).Text));
+            };
+            label.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LeadEntry());
+        }
+    }
+}
